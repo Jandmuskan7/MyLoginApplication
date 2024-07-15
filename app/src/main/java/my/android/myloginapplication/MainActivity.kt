78e33286ce711fcd06918d84a5aacb2d4a1bd7d1
KudratM <mann.kudrat2004@gmail.com>
2024-07-15 14:31:18 -0400

    Simar

--- app/src/main/java/my/android/myloginapplication/MainActivity.kt
+++ app/src/main/java/my/android/myloginapplication/MainActivity.kt
@@ -3,48 +3,48 @@ package my.android.myloginapplication
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import my.android.myloginapplication.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
-    companion object{
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    companion object {
         lateinit var auth: FirebaseAuth
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         auth = FirebaseAuth.getInstance()
-   val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Initialize NavController
+
+
         binding.btnLogin.setOnClickListener {
             val email = binding.editTextTextEmailAddress.text.toString()
             val password = binding.editTextTextPassword.text.toString()
-            if (email.isNotEmpty() && password.isNotEmpty())
-            {
-                MainActivity.auth.signInWithEmailAndPassword(email,password).addOnCompleteListener {
-                    if (it.isSuccessful){
-                        Toast.makeText(this, "Login Successfull",
-                            Toast.LENGTH_LONG).show()
-//intent to home activity
-                        startActivity(
-                            Intent(this, LoginActivity ::
-                        class.java)
-                        )
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                MainActivity.auth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        Toast.makeText(this, "Login Successful", Toast.LENGTH_LONG).show()
+                        // Navigate to home activity
+                        startActivity(Intent(this, LoginActivity::class.java))
                         finish()
                     }
                 }.addOnFailureListener {
-                    Toast.makeText(this, it.localizedMessage,
-                        Toast.LENGTH_LONG).show()
+                    Toast.makeText(this, it.localizedMessage, Toast.LENGTH_LONG).show()
                 }
             }
         }
+
         binding.btnSignUp.setOnClickListener {
-            startActivity(Intent(this, SignUpActivity ::
-            class.java))
+            startActivity(Intent(this, SignUpActivity::class.java))
             finish()
         }
     }
